72bae8814ed6cde476a02f672466ad84dcf305c6
Henrique-CBueno <henriquecb2507@outlook.pt>
2026-02-01 11:34:12 -0300

    ajustando path de seguranca

--- gateway/src/main/java/com/bsys/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/bsys/gateway/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/catalog/movies").hasRole("GATEWAY_ADMIN")
-                        .requestMatchers(HttpMethod.DELETE, "/catalog/movies").hasRole("GATEWAY_ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/catalog/movies/{id}").hasRole("GATEWAY_ADMIN")
                         .requestMatchers("/hello/**").hasRole("GATEWAY_ADMIN")
                         .requestMatchers("/catalog/**").permitAll()
                         .anyRequest().authenticated()
