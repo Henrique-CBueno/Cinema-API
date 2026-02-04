7590043479c2df44d345a20df1fddf17ff2b2f31
Henrique bueno <88509491+Henrique-CBueno@users.noreply.github.com>
2026-02-04 16:25:07 -0300

    feat: add SessionRepository with filtering capabilities for session queries

---
+++ catalog/src/main/java/com/henrique/catalog/repository/SessionRepository.java
@@ -0,0 +1,32 @@
+package com.henrique.catalog.repository;
+
+import com.henrique.catalog.domain.entity.SessionEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Repository
+public interface SessionRepository extends JpaRepository<SessionEntity, UUID> {
+
+    @Query("""
+        SELECT s FROM SessionEntity s
+        WHERE (:movieId IS NULL OR s.movie.id = :movieId)
+        AND (:cinemaId IS NULL OR s.room.cinema.id = :cinemaId)
+        AND (:roomId IS NULL OR s.room.id = :roomId)
+        AND (cast(:date as date) IS NULL OR
+        (s.startTime >= :startOfDay AND s.startTime <= :endOfDay))
+    """)
+    Page<SessionEntity> findSessionsWithFilters(UUID movieId,
+                                                UUID cinemaId,
+                                                UUID roomId,
+                                                LocalDate date, // Usado só para checar null
+                                                LocalDateTime startOfDay,
+                                                LocalDateTime endOfDay,
+                                                Pageable pageable);
+}
