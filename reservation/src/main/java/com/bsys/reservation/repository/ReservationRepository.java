19681cb3fe708a3356cd4b02aceaab5ab3a42ecb
Henrique bueno <88509491+Henrique-CBueno@users.noreply.github.com>
2026-02-07 00:05:58 -0300

    feat: add method to check for existing confirmed reservations

--- reservation/src/main/java/com/bsys/reservation/repository/ReservationRepository.java
+++ reservation/src/main/java/com/bsys/reservation/repository/ReservationRepository.java
@@ -2,10 +2,28 @@ package com.bsys.reservation.repository;
 
 import com.bsys.reservation.domain.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
+
+    @Query("""
+        SELECT CASE\s
+        WHEN COUNT(r) = 0 THEN true
+        ELSE false
+        END
+        FROM Reservation r
+        WHERE r.sessionId = :sessionId
+        AND r.seatId IN :seatIds
+        AND r.status = 'CONFIRMED'
+    """)
+    boolean noConfirmedReservationExists(
+            @Param("sessionId") UUID sessionId,
+            @Param("seatIds") List<UUID> seatIds
+    );
 }
