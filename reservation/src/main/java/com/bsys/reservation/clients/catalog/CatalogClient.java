9431b43a0f8b67c424644742a20b508a9c1918cd
Henrique bueno <88509491+Henrique-CBueno@users.noreply.github.com>
2026-02-06 23:55:35 -0300

    feat: remove CatalogClient interface as it is no longer needed

--- reservation/src/main/java/com/bsys/reservation/clients/catalog/CatalogClient.java
+++
@@ -1,7 +0,0 @@
-package com.bsys.reservation.clients.catalog;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "catalog-client", url = "${clients.catalog.url}")
-public interface CatalogClient {
-}
