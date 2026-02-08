e9c360a68c9dddb0b4e8d52768754ec473030907
Henrique bueno <88509491+Henrique-CBueno@users.noreply.github.com>
2026-02-08 14:50:37 -0300

    feat: add getReservations endpoint for user-specific reservation retrieval

--- reservation/src/main/java/com/bsys/reservation/controller/reservationController.java
+++ reservation/src/main/java/com/bsys/reservation/controller/reservationController.java
@@ -1,10 +1,14 @@
 package com.bsys.reservation.controller;
 
+import com.bsys.reservation.domain.dto.global.PaginationParams;
 import com.bsys.reservation.domain.dto.req.CreateReservationDTO;
+import com.bsys.reservation.domain.dto.res.ReservationResDTO;
+import com.bsys.reservation.infra.padronize.SuccessListDataResponse;
 import com.bsys.reservation.service.ReservationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,5 +33,21 @@ public class reservationController {
                 email,
                 username,
                 userId));
+    }
+
+    @GetMapping()
+    public ResponseEntity<SuccessListDataResponse<ReservationResDTO>> getReservations(@RequestHeader(value = "X-User-Email", required = false) String email,
+                                                                                      @RequestHeader(value = "X-User-Name", required = false) String username,
+                                                                                      @RequestHeader(value = "X-User-Id", required = false) String userId,
+                                                                                      PaginationParams paginationParams) {
+
+        Page<ReservationResDTO> userReservations = reservationService.getUserReservation(UUID.fromString(userId), paginationParams.toPageable());
+
+        if (userReservations.getContent().isEmpty()) return ResponseEntity.noContent().build();
+
+        return ResponseEntity.ok(new SuccessListDataResponse<ReservationResDTO>(userReservations.getContent(),
+                userReservations.getNumber(),
+                userReservations.getSize(),
+                userReservations.getTotalElements()));
     }
 }
