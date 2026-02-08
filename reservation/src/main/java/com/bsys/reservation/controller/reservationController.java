10e12e043bf5693fb3ada30951bf95e9d6c55f5c
Henrique bueno <88509491+Henrique-CBueno@users.noreply.github.com>
2026-02-08 15:03:46 -0300

    feat: refactor reservation retrieval methods for improved code reuse and clarity

--- reservation/src/main/java/com/bsys/reservation/controller/reservationController.java
+++ reservation/src/main/java/com/bsys/reservation/controller/reservationController.java
@@ -8,6 +8,7 @@ import com.bsys.reservation.service.ReservationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jspecify.annotations.NonNull;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,7 +24,7 @@ public class reservationController {
     private final ReservationService reservationService;
 
     @PostMapping()
-    public ResponseEntity<UUID> fazerRezerva(@RequestBody @Valid CreateReservationDTO dto,
+    public ResponseEntity<UUID> makeReservation(@RequestBody @Valid CreateReservationDTO dto,
 
             @RequestHeader(value = "X-User-Email", required = false) String email,
             @RequestHeader(value = "X-User-Name", required = false) String username,
@@ -36,11 +37,25 @@ public class reservationController {
     }
 
     @GetMapping()
-    public ResponseEntity<SuccessListDataResponse<ReservationResDTO>> getReservations(@RequestHeader(value = "X-User-Email", required = false) String email,
-                                                                                      @RequestHeader(value = "X-User-Name", required = false) String username,
-                                                                                      @RequestHeader(value = "X-User-Id", required = false) String userId,
+    public ResponseEntity<SuccessListDataResponse<ReservationResDTO>> getReservations(@RequestHeader(value = "X-User-Id", required = false) String userId,
                                                                                       PaginationParams paginationParams) {
 
+        return getSuccessListDataResponseResponseEntity(userId, paginationParams);
+    }
+
+    @GetMapping("{userId}")
+    public ResponseEntity<SuccessListDataResponse<ReservationResDTO>> getReservationsByUserId(@PathVariable String userId,
+                                                                                      PaginationParams paginationParams) {
+
+        return getSuccessListDataResponseResponseEntity(userId, paginationParams);
+    }
+
+
+
+
+    
+    @NonNull
+    private ResponseEntity<SuccessListDataResponse<ReservationResDTO>> getSuccessListDataResponseResponseEntity(@PathVariable String userId, PaginationParams paginationParams) {
         Page<ReservationResDTO> userReservations = reservationService.getUserReservation(UUID.fromString(userId), paginationParams.toPageable());
 
         if (userReservations.getContent().isEmpty()) return ResponseEntity.noContent().build();
