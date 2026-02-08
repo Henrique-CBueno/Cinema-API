2321562371f368c1ab5006cee1df450140374f57
Henrique bueno <88509491+Henrique-CBueno@users.noreply.github.com>
2026-02-08 17:07:48 -0300

    feat: add payment service with configuration, application, and OpenAPI setup

---
+++ payment/src/main/java/com/bsys/payment/config/OpenApiConfig.java
@@ -0,0 +1,46 @@
+package com.bsys.payment.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.*;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        String securitySchemeName = "oauth2";
+        String authUrl = "http://localhost:9090/realms/auth/protocol/openid-connect/auth";
+        String tokenUrl = "http://localhost:9090/realms/auth/protocol/openid-connect/token";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Payment Service API")
+                        .version("1.0")
+                        .description("API de gerenciamento de catálogo de cinemas e filmes"))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.OAUTH2)
+                                        .flows(new OAuthFlows()
+                                                .authorizationCode(new OAuthFlow()
+                                                        .authorizationUrl(authUrl)
+                                                        .tokenUrl(tokenUrl)
+                                                        .scopes(new Scopes()
+                                                                .addString("openid", "OpenID Connect scope")
+                                                                .addString("profile", "User profile")
+                                                                .addString("email", "User email"))))))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:8080/payment")
+                                .description("Gateway - Ambiente de Desenvolvimento")
+                ));
+    }
+}
