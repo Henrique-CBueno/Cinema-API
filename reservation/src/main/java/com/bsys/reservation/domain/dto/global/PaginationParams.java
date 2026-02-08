110db7bf2d6edf550b2a98f5af3061b5ad22c713
Henrique bueno <88509491+Henrique-CBueno@users.noreply.github.com>
2026-02-08 14:50:18 -0300

    feat: add PaginationParams record for handling pagination parameters

---
+++ reservation/src/main/java/com/bsys/reservation/domain/dto/global/PaginationParams.java
@@ -0,0 +1,21 @@
+package com.bsys.reservation.domain.dto.global;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+public record PaginationParams(Integer page,
+                               Integer pageSize) {
+    public PaginationParams {
+        // Se 'page' for nulo, assume 0. Se for negativo, também (opcional).
+        page = (page == null || page < 0) ? 0 : page;
+
+        // Se 'pageSize' for nulo, assume 10.
+        pageSize = (pageSize == null || pageSize < 1) ? 10 : pageSize;
+    }
+
+    public Pageable toPageable() {
+//        PEGANDO DO QUE FOI CRIADO POR ULTIMO ATE O MAIS VELHO
+        return PageRequest.of(page, pageSize, Sort.by("createdAt").descending());
+    }
+}
