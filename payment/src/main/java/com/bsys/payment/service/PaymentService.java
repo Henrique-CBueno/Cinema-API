c60c3ba88a5872cac161ed1b9cce3959aee2507b
Henrique bueno <88509491+Henrique-CBueno@users.noreply.github.com>
2026-02-11 02:31:58 -0300

    inplementado fluxo de criar customer no gateway

--- payment/src/main/java/com/bsys/payment/service/PaymentService.java
+++ payment/src/main/java/com/bsys/payment/service/PaymentService.java
@@ -4,18 +4,40 @@ import com.bsys.payment.clients.CustomerClient;
 import com.bsys.payment.clients.PaymentClient;
 import com.bsys.payment.clients.dto.BillingRequestDTO;
 import com.bsys.payment.clients.dto.Customer;
+import com.bsys.payment.clients.dto.CustomerClientDTO;
+import com.bsys.payment.clients.dto.UpdateCustomerIdDTO;
 import com.bsys.payment.clients.dto.successResponse.BillingSuccessResponse;
+import com.bsys.payment.clients.dto.successResponse.CustomerSuccessData;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PaymentService {
 
     private final PaymentClient paymentClient;
     private final CustomerClient customerClient;
 
     public Customer createBilling(String userId) {
-        return customerClient.getCustomerById(userId).getBody().data();
+
+        Customer currentCustomer = customerClient.getCustomerById(userId).getBody().data();
+
+        if (currentCustomer.customerId() == null) {
+
+            CustomerSuccessData paymentCustomer = paymentClient.createCustomer(new CustomerClientDTO(
+                    currentCustomer.email(),
+                    currentCustomer.phone(),
+                    currentCustomer.email(),
+                    currentCustomer.taxId()));
+
+            customerClient.updateCustomerId(userId , new UpdateCustomerIdDTO(
+                    paymentCustomer.data().id()
+            ));
+        }
+
+
+        return currentCustomer;
     }
 }
