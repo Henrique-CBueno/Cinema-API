01514824f38f3477a776c8eefc122bc19275ec8d
Henrique bueno <88509491+Henrique-CBueno@users.noreply.github.com>
2026-02-12 15:00:07 -0300

    feat: Implement SNS/SQS messaging for reservation paid event communication between reservation and tickets services.

---
+++ infra/terraform/main.tf
@@ -0,0 +1,69 @@
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    sns = "http://localhost:4566"
+    sqs = "http://localhost:4566"
+  }
+}
+
+# 1. Tópico SNS (O Fan-out)
+resource "aws_sns_topic" "reservation_fanout" {
+  name = "reservation-topic"
+}
+
+# 2. Filas SQS
+resource "aws_sqs_queue" "reservation_paid" {
+  name = "reservation_paid"
+}
+
+resource "aws_sqs_queue" "reservation_notification" {
+  name = "reservation_notification"
+}
+
+# 3. Assinaturas (Conectar SNS -> SQS)
+resource "aws_sns_topic_subscription" "paid_subscription" {
+  topic_arn = aws_sns_topic.reservation_fanout.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.reservation_paid.arn
+  raw_message_delivery = true
+}
+
+resource "aws_sns_topic_subscription" "notification_subscription" {
+  topic_arn = aws_sns_topic.reservation_fanout.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.reservation_notification.arn
+  raw_message_delivery = true
+}
+
+# 4. Políticas de Acesso (Permitir que o SNS envie para o SQS)
+resource "aws_sqs_queue_policy" "sns_to_sqs_policy" {
+  for_each = {
+    paid = aws_sqs_queue.reservation_paid.id
+    notif = aws_sqs_queue.reservation_notification.id
+  }
+
+  queue_url = each.value
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "sqs:SendMessage",
+      "Resource": "*",
+      "Condition": {
+        "ArnEquals": { "aws:SourceArn": "${aws_sns_topic.reservation_fanout.arn}" }
+      }
+    }
+  ]
+}
+POLICY
+}
